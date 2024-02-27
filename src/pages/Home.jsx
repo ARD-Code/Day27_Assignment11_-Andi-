39bebcd0f1815d1b5519db4fab8af91ce18925b5
ARD-Code <andirajadewaa@gmail.com>
2024-02-28 02:36:32 +0700

    menambahkan testimonial pada home , mengganti style bg pada news dan testimonial

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,6 +15,9 @@ import news1 from "../asset_photos/news-1.jpg";
 import news2 from "../asset_photos/a-photo-about-news-restaurant-bakery.jpg";
 import news3 from "../asset_photos/news-2.jpg";
 import news4 from "../asset_photos/news-3.jpg";
+import chef1 from "../asset_photos/chef2.png";
+import chef2 from "../asset_photos/chef1.png";
+import chef3 from "../asset_photos/chef3.png";
 
 const Home = () => {
   useEffect(() => {
@@ -217,9 +220,8 @@ const Home = () => {
       <div
         data-aos="fade-up"
         data-aos-duration="2000"
-        className="container-fluid mt-5"
+        className="container-fluid mt-5 newsSection"
         style={{
-          background: { bgnews2 },
           paddingBottom: "100px",
           border: "none",
         }}
@@ -357,6 +359,140 @@ const Home = () => {
                 Click For More
                 <i className="bi bi-arrow-right-short"></i>
               </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Testi*/}
+
+      <div
+        className="container-fluid mt-0 testimonialSection"
+        style={{
+          height: "1000px",
+          backgroundSize: "cover",
+          border: "none",
+        }}
+      >
+        <div className="container">
+          <div className="row d-flex justify-content-center">
+            <div
+              className="col-md-10 col-xl-8 text-center"
+              style={{ paddingTop: "60px" }}
+            >
+              <h2
+                className="testimoni-h2 poppins-font-bold col-md-"
+                style={{ marginLeft: "50px" }}
+              >
+                Testimoni
+              </h2>
+              <p
+                className="mb-4 pb-2 mb-md-5 pb-md-0 poppins-font-light"
+                style={{ color: "white", fontSize: "20px" }}
+              >
+                "Customers Switching and Leaving Positive Reviews! Explore
+                Impressive Testimonials from Those Who Have Enjoyed an
+                Outstanding Experience with Us."
+              </p>
+            </div>
+          </div>
+
+          <div
+            className="row text-center d-flex align-items-stretch rounded-5"
+            style={{ backgroundColor: "#3f2305", padding: "100px" }}
+          >
+            {/* Testimonial 1 */}
+            <div
+              data-aos="fade-up"
+              data-aos-duration="2000"
+              className="col-md-4 mb-5 mb-md-0 d-flex align-items-stretch"
+            >
+              <div className="card testimonial-card">
+                <div
+                  className="card-up"
+                  style={{ backgroundColor: "#b0a59a" }}
+                ></div>
+                <div className="avatar mx-auto">
+                  <img
+                    src={chef1}
+                    className="rounded-circle img-fluid"
+                    alt="Arnold Poernomo"
+                  />
+                </div>
+                <div className="card-body">
+                  <h4 className="mb-4">Arnold Poernomo</h4>
+                  <hr />
+                  <p className="dark-grey-text mt-4">
+                    <i className="fas fa-quote-left pe-2"></i> "Delightful Bites
+                    Bakery not only crafts delicious pastries but also offers a
+                    cozy ambiance. The attention to detail in their creations
+                    reflects a passion for baking. I'm hooked and can't get
+                    enough!"
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            {/* Testimonial 2 */}
+            <div
+              data-aos="fade-up"
+              data-aos-duration="2000"
+              className="col-md-4 mb-5 mb-md-0 d-flex align-items-stretch"
+            >
+              <div className="card testimonial-card">
+                <div
+                  className="card-up"
+                  style={{ backgroundColor: "#b0a59a" }}
+                ></div>
+                <div className="avatar mx-auto bg-white">
+                  <img
+                    src={chef2}
+                    className="rounded-circle img-fluid"
+                    alt="Renatta Olivia Moeloek"
+                  />
+                </div>
+                <div className="card-body">
+                  <h4 className="mb-4">Renatta Olivia Moeloek</h4>
+                  <hr />
+                  <p className="dark-grey-text mt-4">
+                    <i className="fas fa-quote-left pe-2"></i> "As a regular
+                    customer, I can confidently say that Delightful Bites Bakery
+                    is a slice of heaven. The warmth of the staff, coupled with
+                    their delectable pastries, makes it my go-to spot for all
+                    things sweet."
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            {/* Testimonial 3 */}
+            <div
+              data-aos="fade-up"
+              data-aos-duration="2000"
+              className="col-md-4 mb-0 d-flex align-items-stretch"
+            >
+              <div className="card testimonial-card">
+                <div
+                  className="card-up"
+                  style={{ backgroundColor: "#b0a59a" }}
+                ></div>
+                <div className="avatar mx-auto bg-white">
+                  <img
+                    src={chef3}
+                    className="rounded-circle img-fluid"
+                    alt="Juna Rorimpandey"
+                  />
+                </div>
+                <div className="card-body">
+                  <h4 className="mb-4">Juna Rorimpandey</h4>
+                  <hr />
+                  <p className="dark-grey-text mt-4">
+                    <i className="fas fa-quote-left pe-2"></i> "The aroma of
+                    freshly baked goods at Delightful Bites Bakery is
+                    irresistible! Every treat I've tried has been a taste of
+                    perfection. A true gem for pastry lovers!"
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
